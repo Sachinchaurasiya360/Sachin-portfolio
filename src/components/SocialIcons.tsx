48566bdebd2b9269a1eff0217d06b3114df77f27
Sachin Chaurasiya <mrsachinchaurasiya@gmail.com>
2026-03-03 19:10:10 +0530

    fix build issues

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,9 +4,7 @@ import {
   FaXTwitter,
 } from "react-icons/fa6";
 import "./styles/SocialIcons.css";
-import { TbNotes } from "react-icons/tb";
 import { useEffect } from "react";
-import HoverLinks from "./HoverLinks";
 import { config } from "../config";
 
 const SocialIcons = () => {
